c34174a35837a0e8aa7134b2bf3f1e4ed9ffe154
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-08-01 08:58:48 +0100

    get data from GPIOs and send with echo

--- src/beanport.c
+++ src/beanport.c
@@ -2,11 +2,26 @@
 #include <ctype.h>
 #include <stdio.h>
 
+// GP0-GP7 = D0-D7 from the level shifter's B-side (design.md §10)
+static uint8_t read_data_bus(void) {
+    uint8_t value = 0;
+    for (int i = 0; i < 8; i++) {
+        value |= gpio_get(i) << i;
+    }
+    return value;
+}
+
 int main() {
     stdio_init_all();
+
+    for (int i = 0; i < 8; i++) {
+        gpio_init(i);
+        gpio_set_dir(i, GPIO_IN);
+    }
 
     while (true) {
         int c = getchar();
         putchar(toupper(c));
+        printf(" %02X ", read_data_bus());
     }
 }
