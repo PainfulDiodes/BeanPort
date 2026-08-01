5a988ad9a60b4d3075e36728a5759048c85cd1a4
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-08-01 07:33:45 +0100

    USB console echo

--- src/beanport.c
+++ src/beanport.c
@@ -1,31 +1,11 @@
 #include "pico/stdlib.h"
-#ifdef CYW43_WL_GPIO_LED_PIN
-#include "pico/cyw43_arch.h"
-#endif
+#include <stdio.h>
 
 int main() {
-#if defined(PICO_DEFAULT_LED_PIN)
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-    if (cyw43_arch_init()) {
-        return -1;
-    }
-#endif
+    stdio_init_all();
 
     while (true) {
-#if defined(PICO_DEFAULT_LED_PIN)
-        gpio_put(LED_PIN, true);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
-#endif
-        sleep_ms(250);
-#if defined(PICO_DEFAULT_LED_PIN)
-        gpio_put(LED_PIN, false);
-#elif defined(CYW43_WL_GPIO_LED_PIN)
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
-#endif
-        sleep_ms(250);
+        int c = getchar();
+        putchar(c);
     }
 }
